f4c2ed554894b2ca54f6d0d44ad3c4e82b0a3161
egemerd <egemertkarsli@hotmail.com>
2026-08-17 04:08:19 +0300

    wall bounce animation fixed.

--- 3d-topdown-action/Assets/Scripts/GameFeel/WallBounceEffect.cs
+++ 3d-topdown-action/Assets/Scripts/GameFeel/WallBounceEffect.cs
@@ -5,9 +5,33 @@ using System;
 public class WallBounceEffect : MonoBehaviour
 {
     [SerializeField] WallBounceEffectConfigSO config;
+
+    private Vector3 originalScale;
+    private Vector3 originalPosition;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+        originalPosition = transform.localPosition;
+    }
+
     public void BounceEffect()
     {
+        // Önceki tween'leri temizle ve reset
+        transform.DOKill();
+        transform.localScale = originalScale;
+        transform.localPosition = originalPosition;
+
+        // Þimdi yeni tween'i baþlat
         transform.DOPunchScale(config.bounceForce, config.bounceDuration, config.bounceVibrato, config.randomness);
         transform.DOShakePosition(config.positionDuration, config.positionForce, config.positionVibrato, config.positionRandomness);
+    }
+
+    private void OnDisable()
+    {
+        // Obje disable olursa tween takýlý kalmasýn
+        transform.DOKill();
+        transform.localScale = originalScale;
+        transform.localPosition = originalPosition;
     }
 }
